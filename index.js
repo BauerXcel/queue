dbe770e519b0c0280f5b300d5bfd8eff083f9adb
Ross Pickerill <ross.pickerill@bauermedia.co.uk>
2020-11-19 16:20:58 +0000

    Change to name of package for deploying fork to our npm registry.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const QueueHandler = require('./lib/queue-handler')
-
+console.log('--->',__dirname + '/config')
 module.exports.Config = require(__dirname + '/config')
 module.exports = new QueueHandler()
